a59113c9c89cac809b66ad8ee72e8695a315b26f
afterreal <117338983+afterreal@users.noreply.github.com>
2023-01-23 08:52:11 +0000

    text update

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 
 <template>
   <nav class="flex justify-center space-x-4 p-2 bg-slate-100">
+  <div>
+    <p>The One</p>
+  </div>
     <a
       href="/dashboard"
       class="font-bol px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-300 hover:text-slate-900"
