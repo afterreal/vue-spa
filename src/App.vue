9e854a4870d6b97c6be5f2ae846b0f7a4301f973
callazar <callazar1234@gmail.com>
2023-01-23 11:10:59 +0700

    update, jan 23, 11:10

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
- <div class="text-3xl font-bold p-2 bg-slate-300">LOGO</div>
- <button class="rounded-full p-1 pl-3 pr-3 font-sans font-medium bg-sky-500 hover:bg-sky-400">SEE ALL</button>
+  <div class="text-3xl font-bold p-2 bg-slate-300">LOGO</div>
+  <button class="rounded-full p-1 pl-3 pr-3 font-sans font-medium bg-sky-500 hover:bg-sky-400">
+    SEE ALL
+  </button>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
